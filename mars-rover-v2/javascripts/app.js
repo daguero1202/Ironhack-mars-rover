9bc544f1e436559f1e4fa0205f7adac257685382
David Aguero <22003@columbushs.com>
2019-11-13 09:13:17 -0500

    finished

--- mars-rover-v2/javascripts/app.js
+++ mars-rover-v2/javascripts/app.js
@@ -1,45 +1,61 @@
 // Rover Object Goes Here
 // ======================
-rover = {direction: "N", x: 0, y: 0}
+let rover = { direction: "N", x: 0, y: 0,  travelLog: []}
 // ======================
-function turnLeft(rover){
-  if (rover.direction == "N"){
+function turnLeft(rover) {
+  if (rover.direction == "N") {
     rover.direction = "W";
-  } else if(rover.direction == "W"){
+  } else if (rover.direction == "W") {
     rover.direction = "S";
-  } else if(rover.direction == "S"){
-    rover.direciton = "E";
-  } else if(rover.direction == "E"){
-    rover.direciton = "N";
+  } else if (rover.direction == "S") {
+    rover.direction = "E";
+  } else if (rover.direction == "E") {
+    rover.direction = "N";
   }
   console.log("turnLeft was called!");
 }
 
-function turnRight(rover){
-  if (rover.direction == "N"){
+function turnRight(rover) {
+  if (rover.direction == "N") {
     rover.direction = "E";
-  } else if(rover.direction == "E"){
+  } else if (rover.direction == "E") {
     rover.direction = "S";
-  } else if(rover.direction == "S"){
-    rover.direciton = "W";
-  } else if(rover.direction == "W"){
-    rover.direciton = "N";
+  } else if (rover.direction == "S") {
+    rover.direction = "W";
+  } else if (rover.direction == "W") {
+    rover.direction = "N";
   }
   console.log("turnRight was called!");
 }
 
-function moveForward(rover){
-  if (rover.direction == "N"){
+function moveForward(rover) {
+  if (rover.direction == "N") {
     rover.y = rover.y + 1;
-  } else if(rover.direction == "W"){
+  } else if (rover.direction == "W") {
     rover.x = rover.x - 1;
-  } else if(rover.direction == "S"){
+  } else if (rover.direction == "S") {
     rover.y = rover.y - 1;
-  } else if(rover.direction == "E"){
+  } else if (rover.direction == "E") {
     rover.x = rover.x + 1;
   }
   console.log("moveForward was called")
 }
-function moving(){
 
+let commands = ["r", "r", "f","r","l","f", "r", "f", "f", "l"]
+
+function moving(drive) {
+  drive.forEach( ( eachletter ) => {
+    if (eachletter == "f") {
+      moveForward(rover);
+    } else if (eachletter == "r") {
+      turnRight(rover);
+    } else if (eachletter == "l") {
+      turnLeft(rover);
+    }
+    rover.travelLog.push(`Coordinates: ${rover.x}, ${rover.y}`);
+  })
 }
+
+
+
+
